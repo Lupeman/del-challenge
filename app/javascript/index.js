509072884f2d515796f3e11e44a25cc8ea7e4fea
Lupeman <jaimegunther@hotmail.com>
2017-03-24 14:41:00 +1100

    header and wip carousel

---
+++ app/javascript/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+$('.carousel').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    prevArrow: <i class="fa fa-angle-left" aria-hidden="true"></i>,
+    nextArrow: <i class="fa fa-angle-right" aria-hidden="true"></i>
+    responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
+  });
+});
